264160457208a40f44f7dc00dc37635314335c24
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-09 16:26:06 +0100

    add input and conect to js

--- asset/js/main.js
+++ asset/js/main.js
@@ -8,16 +8,22 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 
 //chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-const kilometersToGo = prompt ("Quanti chilometri vuoi percorrere ?");
-const userAge = prompt ("quanti anni hai?");
-console.log(kilometersToGo, userAge);
+const sendEl = document.getElementById("send")
+let kilometersToGoValue = document.getElementById("kilometersToGo")
+let userAgeValue = document.getElementById("userAge")
+sendEl.addEventListener('click', function () {
+    kilometersToGoEl = document.getElementById("kilometersToGo").value
+    userAgeEl = document.getElementById("userAge").value
+})
+
+console.log(kilometersToGoValue, userAgeValue);
 
 //Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio
 const costForKm = 0.21;
 console.log(costForKm);
 
 //il prezzo del biglietto è definito in base ai km (0.21 € al km)
-const costTot = costForKm * kilometersToGo;
+const costTot = costForKm * kilometersToGoValue;
 console.log(costTot);
 
 //va applicato uno sconto del 20% per i minorenni
@@ -25,11 +31,11 @@ let costTotWhitSald;
 costTotWhitSald = costTot;
 const saldMinors = 20;
 const saldOver = 40;
-if(userAge <= 18) {
-    costTotWhitSald = costTot - ((costTot * saldMinors) / 100) ;
+if (userAgeValue <= 18) {
+    costTotWhitSald = costTot - ((costTot * saldMinors) / 100);
 
-//va applicato uno sconto del 40% per gli over 65.
-} else if(userAge >= 65) {
+    //va applicato uno sconto del 40% per gli over 65.
+} else if (userAgeValue >= 65) {
     costTotWhitSald = costTot - ((costTot * saldOver) / 100);
 }
 
