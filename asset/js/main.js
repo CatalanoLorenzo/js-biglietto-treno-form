4053ecf793b8eb78f88085719beabe89768db3a6
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-09 19:22:22 +0100

    add element show in ticket section

--- asset/js/main.js
+++ asset/js/main.js
@@ -29,24 +29,33 @@ sendEl.addEventListener('click', function () {
     costTotWhitSald = costTot;
     const saldMinors = 20;
     const saldOver = 40;
+    let typeOfSald = "Standard"
     if (userAgeValue == 'Minorenne') {
-        console.log(`age user ${userAgeValue}`)
+        typeOfSald = "Ridotto"
         costTotWhitSald = costTot - ((costTot * saldMinors) / 100);
+        console.log(`age user ${userAgeValue} type of ticket ${typeOfSald}`)
     
         //va applicato uno sconto del 40% per gli over 65.
     } else if (userAgeValue == 'Over60') {
-        console.log(`age user ${userAgeValue}`)
+        typeOfSald = "Ridotto"
         costTotWhitSald = costTot - ((costTot * saldOver) / 100);
+        console.log(`age user ${userAgeValue} type of ticket ${typeOfSald}`)
     } else if (userAgeValue == 'Maggiorenne'){
-        console.log(`age user ${userAgeValue}`)
+        console.log(`age user ${userAgeValue} type of ticket ${typeOfSald}`)
     }
     
     //L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo)
     costTotWhitSald = costTotWhitSald.toFixed(2);
     let costo = document.getElementById("costo_display");
+    let passegerName = document.getElementById("userName_display")
+    let ticketType = document.getElementById ("typeSald")
+    let numberWagon = document.getElementById("numberWagon")
     console.log(`cost with applicaed sald ${costTotWhitSald}`);
     costo.innerHTML = costTotWhitSald;
-
+    passegerName.innerHTML = userNameValue
+    ticketType.innerHTML = typeOfSald
+    numberWagon.innerHTML = Math.floor((Math.random()*10) + 1)
+    codeCP.innerHTML = Math.floor((Math.random()*1000) + 1)
 })
 reset.addEventListener('click', function () {
     document.getElementById("userName").value = ""
